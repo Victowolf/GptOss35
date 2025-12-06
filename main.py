a0c44cb855a60c27b48223847d0e53d4979328a3
VictoWolf <hosalliyash@gmail.com>
2025-12-06 19:45:50 +0530

    main update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from transformers import pipeline
 
 # --------------------------------------------------------------------
 # Disable FlashAttention if your hardware does not support it
-# --------------------------------------------------------------------
+# -------------------------------------------------------------------- 
 os.environ.setdefault("FLASH_ATTENTION", "1")
 os.environ.setdefault("DISABLE_FLASH_ATTENTION", "0")
 os.environ.setdefault("HF_DISABLE_FLASH_ATTENTION", "0")
